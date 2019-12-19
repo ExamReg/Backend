b166ec3cb1e149a437c368627abadadaa5e0e4c7
quanht <zed.quan99@gmail.com>
2019-12-19 20:30:14 +0700

    commit to pull

--- router/index.js
+++ router/index.js
@@ -1,11 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const Guest = require("../controllers/guest/guest");
-
 router.use("/a", require("./admin_router"));
 router.use("/s", require("./student_router"));
-
-router.get("/g/semesters", Guest.getSemester);
 
 module.exports = router;
