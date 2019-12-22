2a0e001750e2fcbbb66fbebbcb007e02b5ef754b
quanht <zed.quan99@gmail.com>
2019-12-22 17:40:50 +0700

    get exams

--- controllers/student/exam.js
+++ controllers/student/exam.js
@@ -2,40 +2,61 @@ const db = require("../../models/index");
 const response = require("../../utils/response");
 
 async function getExams(req, res){
+    let {id_semester} = req.query;
+    //check co trong time dang ky hay k
     try{
+        if(!id_semester){
+            throw new Error("Vui lòng chọn một kỳ học.")
+        }
         let sql = "SELECT \n" +
-            "    CSe.*,\n" +
+            "    S.time_start,\n" +
+            "    S.time_end,\n" +
+            "    R.location,\n" +
+            "    S.maximum_seating,\n" +
             "    (SELECT \n" +
             "            course_name\n" +
             "        FROM\n" +
             "            Course\n" +
             "        WHERE\n" +
-            "            CSe.id_course = id_course) course_name,\n" +
-            "    S.*,\n" +
+            "            id_course = CSe.id_course) AS course_name,\n" +
+            "    S.id AS id_slot,\n" +
+            "    CSe.id_cs,\n" +
             "    (SELECT \n" +
-            "            location\n" +
+            "            is_eligible\n" +
             "        FROM\n" +
-            "            Room\n" +
+            "            CourseStudent\n" +
             "        WHERE\n" +
-            "            id_room = S.id_room) AS location,\n" +
-            "    COUNT(SS.id_student) AS seated\n" +
+            "            id_student = :id_student\n" +
+            "                AND id_cs = CSe.id_cs) as is_eligible,\n" +
+            "    sum(if(SS.id_student is not null, 1, 0)) as totalRegistered\n" +
             "FROM\n" +
+            "    (SELECT \n" +
+            "        *\n" +
+            "    FROM\n" +
+            "        CourseStudent\n" +
+            "    WHERE\n" +
+            "        id_student = :id_student) AS CSt\n" +
+            "        INNER JOIN\n" +
             "    (SELECT \n" +
             "        *\n" +
             "    FROM\n" +
             "        CourseSemester\n" +
             "    WHERE\n" +
-            "        CourseSemester.is_done IS FALSE) CSe\n" +
+            "        id_semester = :id_semester) AS CSe on CSt.id_cs = CSe.id_cs\n" +
             "        INNER JOIN\n" +
             "    Exam E ON E.id_cs = CSe.id_cs\n" +
             "        INNER JOIN\n" +
             "    Slot S ON S.id_exam = E.id_exam\n" +
             "        LEFT JOIN\n" +
             "    StudentSlot SS ON SS.id_slot = S.id\n" +
-            "GROUP BY S.id;";
+            "        INNER JOIN\n" +
+            "    Room R ON R.id_room = S.id_room\n" +
+            "GROUP BY S.id" +
+            " order by course_name";
         let exams = await db.sequelize.query(sql, {
             replacements: {
-
+                id_semester: id_semester,
+                id_student: req.tokenData.id_student
             },
             type: db.Sequelize.QueryTypes.SELECT
         });
@@ -54,34 +75,35 @@ async function getExamsRegistered(req, res){
             throw new Error("Vui lòng chọn 1 kỳ học.")
         }
         let sql = "SELECT \n" +
-            "    Slot.*,\n" +
-            "    CSe.id_course,\n" +
-            "    (SELECT \n" +
-            "            course_name\n" +
-            "        FROM\n" +
-            "            Course\n" +
-            "        WHERE\n" +
-            "            id_course = CSe.id_course) as course_name\n" +
+            "    C.course_name,\n" +
+            "    C.id_course,\n" +
+            "    S.time_start,\n" +
+            "    S.time_end,\n" +
+            "    S.id AS id_slot,\n" +
+            "    R.location\n" +
             "FROM\n" +
-            "    (SELECT \n" +
-            "        *\n" +
-            "    FROM\n" +
-            "        CourseSemester\n" +
-            "    WHERE\n" +
-            "        id_semester = :id_semester) CSe\n" +
-            "        INNER JOIN\n" +
-            "    CourseStudent CSt ON CSt.id_cs = CSe.id_cs\n" +
-            "        INNER JOIN\n" +
-            "    Exam E ON E.id_cs = CSe.id_cs\n" +
-            "        INNER JOIN\n" +
-            "    Slot ON Slot.id = E.id_exam\n" +
-            "        INNER JOIN\n" +
             "    (SELECT \n" +
             "        *\n" +
             "    FROM\n" +
             "        StudentSlot\n" +
             "    WHERE\n" +
-            "        id_student = :id_student) SS ON SS.id_slot = Slot.id;\n"
+            "        id_student = :id_student) SS\n" +
+            "        INNER JOIN\n" +
+            "    Slot AS S ON S.id = SS.id_slot\n" +
+            "        INNER JOIN\n" +
+            "    Exam E ON E.id_exam = S.id_exam\n" +
+            "        INNER JOIN\n" +
+            "    (SELECT \n" +
+            "        *\n" +
+            "    FROM\n" +
+            "        CourseSemester\n" +
+            "    WHERE\n" +
+            "        id_semester = :id_semester) CS ON CS.id_cs = E.id_cs\n" +
+            "        INNER JOIN\n" +
+            "    Course C ON C.id_course = CS.id_course\n" +
+            "        INNER JOIN\n" +
+            "    Room R ON S.id_room = R.id_room" +
+            "   order by S.time_start ASC";
         let exams = await db.sequelize.query(sql, {
             replacements: {
                 id_student: req.tokenData.id_student,
@@ -93,7 +115,7 @@ async function getExamsRegistered(req, res){
     }
     catch(err){
         console.log("getExamsRegistered: ", err.message);
-
+        return res.json(response.buildFail(err.message));
     }
 }
 
